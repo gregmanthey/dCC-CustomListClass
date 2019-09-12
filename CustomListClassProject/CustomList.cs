0f1047cfacc0622249447357ae881cb4883dad06
gregmanthey <gmanthey03@gmail.com>
2019-09-12 07:00:36 -0500

    In Unit Tests, added tests for Remove method. In CustomList, added Capacity property and capacity member variable, as well as Remove method. Added some methods to break down the logic in Add method. Set default count and capacity in Constructor.

--- CustomListClassProject/CustomList.cs
+++ CustomListClassProject/CustomList.cs
@@ -10,33 +10,81 @@ namespace CustomListClassProject
   {
     private T[] items;
     private int count;
+    private int capacity;
     public int Count { get => count; }
+    public int Capacity { get => capacity; }
     public T this[int i] { get => items[i]; set => items[i] = value; }
 
     public CustomList()
     {
       items = new T[4];
+      count = 0;
+      capacity = 4;
     }
 
     public void Add(T item)
     {
-      if (count == items.Length)
+      if (ListIsAtCapacity())
       {
-        int newArrayLength = items.Length * 2;
-        T[] temporaryArray = new T[newArrayLength];
-        for (int i = 0; i < count; i++)
-        {
-          temporaryArray[i] = items[i];
-        }
-        items = new T[newArrayLength];
-        for (int i = 0; i < count; i++)
-        {
-          items[i] = temporaryArray[i];
-        }
+        DoubleCapacity();
+        T[] temporaryArray = new T[capacity];
+        CopyArrayOneToArrayTwo(items, temporaryArray);
+        items = new T[capacity];
+        CopyArrayOneToArrayTwo(temporaryArray, items);
       }
 
       items[count] = item;
       count++;
+    }
+
+    public void Remove(T item)
+    {
+      CustomList<T> temporaryList = new CustomList<T>();
+      for (int i = 0; i < count; i++)
+      {
+        if (items[i].Equals(item))
+        {
+          continue;
+        }
+        else
+        {
+          temporaryList.Add(items[i]);
+        }
+      }
+      items = new T[capacity];
+      count = 0;
+      for (int i = 0; i < temporaryList.Count; i++)
+      {
+        items[i] = temporaryList[i];
+        count++;
+      }
+    }
+
+    private bool ListIsAtCapacity()
+    {
+      if (count == capacity)
+      {
+        return true;
+      }
+      return false;
+    }
+
+    private void DoubleCapacity()
+    {
+      capacity *= 2;
+    }
+
+    private void CopyArrayOneToArrayTwo(T[] arrayOne, T[] arrayTwo)
+    {
+      for (int i = 0; i < count; i++)
+      {
+        arrayTwo[i] = arrayOne[i];
+      }
+    }
+
+    private void PointReferenceFromOldArrayToNewOne(T[] temporaryArray)
+    {
+      items = temporaryArray;
     }
   }
 }
--- CustomListClassUnitTests/CustomListClassUnitTests.cs
+++ CustomListClassUnitTests/CustomListClassUnitTests.cs
@@ -52,6 +52,7 @@ namespace CustomListClassUnitTests
       CustomList<int> testList = new CustomList<int>();
       int expected = 42;
       int actual;
+      int index = 8;
 
       //act
       testList.Add(166);
@@ -61,9 +62,9 @@ namespace CustomListClassUnitTests
       testList.Add(42);
       testList.Add(33);
       testList.Add(99);
+      testList.Add(55);
       testList.Add(42);
-      testList.Add(42);
-      actual = testList[8];
+      actual = testList[index];
 
       //assert
       Assert.AreEqual(expected, actual);
@@ -132,5 +133,331 @@ namespace CustomListClassUnitTests
       //assert
       Assert.AreEqual(expected, actual);
     }
+
+
+    [TestMethod]
+    public void Remove_RemoveItemFromEmptyList_DoNothingToCount()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 0;
+      int actual;
+
+      //act
+      testList.Remove(42);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveItemFromEmptyList_DoNothingToCapacity()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 4;
+      int actual;
+
+      //act
+      testList.Remove(42);
+      actual = testList.Capacity;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveItemThatIsNotInAListWithFiveItems_DoNothingToCount()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 5;
+      int actual;
+
+      //act
+      testList.Add(1);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfOne_CountBecomesZero()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 0;
+      int actual;
+
+      //act
+      testList.Add(42);
+      testList.Remove(42);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfFive_CountBecomesFour()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 4;
+      int actual;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfNine_CountBecomesEight()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 8;
+      int actual;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Add(6);
+      testList.Add(7);
+      testList.Add(8);
+      testList.Add(9);
+      testList.Remove(42);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    //[TestMethod]
+    //[ExpectedException(typeof(IndexOutOfRangeException))]
+    //public void Remove_RemoveASingleItemFromListWithLengthOfOneAndTryAccessingIndexZero_ThrowIndexOutOfRangeException()
+    //{
+    //  //arrange
+    //  CustomList<int> testList = new CustomList<int>();
+    //  int index = 0;
+    //  int number;
+
+    //  //act
+    //  testList.Add(42);
+    //  testList.Remove(42);
+    //  number = testList[index];
+    //}
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfFive_IndexOneBecomesIndexZero()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 2;
+      int actual;
+      int index = 0;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      actual = testList[index];
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfFive_IndexTwoBecomesIndexOne()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 3;
+      int actual;
+      int index = 1;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      actual = testList[index];
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveItemsAtIndicesZeroAndTwoFromListWithLengthOfFive_IndexFourBecomesIndexTwo()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 5;
+      int actual;
+      int index = 2;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      testList.Remove(3);
+      actual = testList[index];
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveAllFiveItemsFromListWithLengthOfFive_CountAtZero()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 0;
+      int actual;
+
+      //act
+      testList.Add(42);
+      testList.Add(3);
+      testList.Add(3);
+      testList.Add(42);
+      testList.Add(3);
+      testList.Remove(42);
+      testList.Remove(3);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveAllFiveItemsFromListWithLengthOfFiveThenAddThreeItems_CountAtThree()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 3;
+      int actual;
+
+      //act
+      testList.Add(42);
+      testList.Add(3);
+      testList.Add(3);
+      testList.Add(42);
+      testList.Add(3);
+      testList.Remove(42);
+      testList.Remove(3);
+      testList.Add(25);
+      testList.Add(81);
+      testList.Add(242);
+      actual = testList.Count;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfFive_IndexThreeBecomesIndexTwo()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 4;
+      int actual;
+      int index = 2;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      actual = testList[index];
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    [TestMethod]
+    public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfFive_IndexFourBecomesIndexThree()
+    {
+      //arrange
+      CustomList<int> testList = new CustomList<int>();
+      int expected = 5;
+      int actual;
+      int index = 3;
+
+      //act
+      testList.Add(42);
+      testList.Add(2);
+      testList.Add(3);
+      testList.Add(4);
+      testList.Add(5);
+      testList.Remove(42);
+      actual = testList[index];
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+
+    //[TestMethod]
+    //[ExpectedException(typeof(IndexOutOfRangeException))]
+    //public void Remove_RemoveASingleItemFromFrontOfListWithLengthOfFive_IndexFourThrowsIndexOutOfRangeException()
+    //{
+    //  //arrange
+    //  CustomList<int> testList = new CustomList<int>();
+    //  int number;
+    //  int index = 4;
+
+    //  //act
+    //  testList.Add(42);
+    //  testList.Add(2);
+    //  testList.Add(3);
+    //  testList.Add(4);
+    //  testList.Add(5);
+    //  testList.Remove(42);
+    //  number = testList[index];
+    //}
+
+
+
   }
 }
